219235282193d743f2a24c01f95b4ce9c6217085
tamercos <tameryc@outlook.com>
2018-01-18 13:52:45 +0000

    added a comment (still practicing with github)

--- Main.cpp
+++ Main.cpp
@@ -4,8 +4,9 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello team16, night night" << endl;
+	cout << "Hello team16" << endl;
 
+	//cin.ignore() allows command prompt to stay up until a key is pressed
 	cin.ignore();
 
 	return 0;
