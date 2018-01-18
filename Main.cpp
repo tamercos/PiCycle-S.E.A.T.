bf12664d4d36e7075045aebfbbb2525d2055e5e2
paoloerc <35561543+paoloerc@users.noreply.github.com>
2018-01-18 14:04:01 +0000

    Made it More complicated

--- Main.cpp
+++ Main.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
-	cout << "Hello team16" << endl;
+	cout << "Hello team16, Please press enter to continue with this exercise" << endl;
 
 	//cin.ignore() allows command prompt to stay up until a key is pressed
 	cin.ignore();
 
 	//I'll break yo code with this line' --> fixed it
+
+	int firstNumber, secondNumber, sumOfTwoNumbers;
+
+	cout << "Enter two integers: ";
+	cin >> firstNumber >> secondNumber;
+
+	// sum of two numbers in stored in variable sumOfTwoNumbers
+	sumOfTwoNumbers = firstNumber + secondNumber;
+
+	// Prints sum 
+	cout << firstNumber << " + " << secondNumber << " = " << sumOfTwoNumbers;
+
 	return 0;
 }
